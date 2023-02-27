e6fbc00852cb0b8b91c9a8a9c5f5d620acf29c31
Jan Breig <git@pygos.space>
2023-02-27 15:00:43 +0100

    Update build script

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd container
-docker build -t ultimator14/pwn-docker:latest .
+docker pull archlinux:latest
+docker build --no-cache -t ultimator14/pwn-docker:latest .
 docker push ultimator14/pwn-docker:latest
 cd ..
